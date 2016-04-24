0606fd6982fcc7516df6e44e426597ed1b776273
Jean-Claude Paquin <jcrpaquin@yahoo.com>
2016-04-24 12:35:51 -0400

    Initial commit

---
+++ src/main/java/io/github/magisun/graphmaster/Launch.java
@@ -0,0 +1,19 @@
+package io.github.magisun.graphmaster;
+
+/**
+ * The GraphMaster entrypoint:
+ * - Initiates GUI construction
+ * - Opens static resources
+ * - Prepares scripting engines
+ */
+public class Launch {
+
+    /**
+     * The main program entrypoint.
+     *
+     * @param args Program arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
